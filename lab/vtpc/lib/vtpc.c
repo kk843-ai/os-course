44eacfe672a91e3bae7a49eaf29463723bd855d6
kk843-ai <kk.843@mail.ru>
2025-12-17 19:44:14 +0300

    Update vtpc.c
    Signed-off-by: kk843-ai <kk.843@mail.ru>

--- lab/vtpc/lib/vtpc.c
+++ lab/vtpc/lib/vtpc.c
@@ -1,30 +1,27 @@
 #include "vtpc.h"
 
-#include <fcntl.h>
-#include <stddef.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include "vtpc_impl.h"
 
 int vtpc_open(const char* path, int mode, int access) {
-  return open(path, mode, access);
+  return vtpc_impl_open(path, mode, access);
 }
 
 int vtpc_close(int fd) {
-  return close(fd);
+  return vtpc_impl_close(fd);
 }
 
 ssize_t vtpc_read(int fd, void* buf, size_t count) {
-  return read(fd, buf, count);
+  return vtpc_impl_read(fd, buf, count);
 }
 
 ssize_t vtpc_write(int fd, const void* buf, size_t count) {
-  return write(fd, buf, count);
+  return vtpc_impl_write(fd, buf, count);
 }
 
 off_t vtpc_lseek(int fd, off_t offset, int whence) {
-  return lseek(fd, offset, whence);
+  return vtpc_impl_lseek(fd, offset, whence);
 }
 
 int vtpc_fsync(int fd) {
-  return fsync(fd);
+  return vtpc_impl_fsync(fd);
 }
